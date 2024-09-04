ad81489501d33655d8ba9459134fef99585f4310
Rafael Santos Teixeira <rafaelsantost87@gmail.com>
2024-09-04 14:56:33 -0300

    logica de imc

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Text, TextInput, View } from 'react-native'
+import ResultImc from './ResultImc/'
 
 export default function Form(){
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("Preencha o peso e a altura")
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState('Calcular')
+
+    function imcCalculator(){
+        return setImc((weight/(height*height)).toFixed())
+    }
+
+    function validationImc(){
+        if(weight != null && height != null){
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual a:")
+            setTextButton("Calcular Novamente")
+            return
+        }
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("Preecha o peso e a altura")
+    }
+
     return (
         <View>
             <View>
                 <Text>Altura</Text>
                 <TextInput
+                    onChangeText={setHeight}
+                    value={height}
                     placeholder='Ex. 1.75'
                     keyboardType='numeric'
                 />
                 <Text>Peso</Text>
                 <TextInput
+                    onChangeText={setWeight}
+                    value={weight}
                     placeholder='Ex. 75.365'
                     keyboardType='numeric'
                 />
                 <Button
-                    title='Calcular IMC'
+                    onPress={() => validationImc()}
+                    title={textButton}
                 />
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
